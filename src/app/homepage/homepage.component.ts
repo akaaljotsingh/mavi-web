21bf253298d2ea793a18d9918e4fd9c78f2f939e
Akaaljot Singh <akaaljotsingh@gmail.com>
2018-08-29 14:16:33 +0530

    sticky header, animation

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-homepage',
@@ -9,9 +10,31 @@ export class HomepageComponent implements OnInit {
 
   showNavigationIndicators:any;
 
-  constructor() { }
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 3000;
+    config.pauseOnHover = false;
+  }
 
   ngOnInit() {
+    
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("navbar1");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
   }
 
 }
